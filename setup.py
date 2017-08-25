8cea6d3430c91795d950625ebcf1916510394cb6
Daniel Kiss <littlesnorrboy@gmail.com>
2017-08-25 21:29:49 +0200

    Move the deployment preparation to script
    Move the deployment preparation to script
    to get feedback about the build's status

--- setup.py
+++ setup.py
@@ -2,38 +2,50 @@
 
 import os
 import re
+import sys
 from setuptools import setup, find_packages
 
-try:
-  import pandoc
-  doc = pandoc.Document()
-  with open("README.md") as file:
-    doc.markdown = file.read()
-    home_page = doc.rst
-except:
-  home_page = "Something went wrong while generating this README. Please refer to https://github.com/snorrwe/frenetiq-crawler"
+def main():
+  result = 0
+  try:
+    import pandoc
+    doc = pandoc.Document()
+    with open("README.md") as file:
+      doc.markdown = file.read()
+      home_page = doc.rst
+  except:
+    print("Something went wrong while generating the README!")
+    result += 1
+    home_page = "Something went wrong while generating the README. Please refer to https://github.com/snorrwe/frenetiq-crawler"
 
-try:
-  tag = os.environ['TRAVIS_TAG']
-  re.search(r'(\d\.){3,}', tag).group(0)
-except:
-  version = 'UNKNOWN_VERSION'
+  try:
+    tag = os.environ['TRAVIS_TAG']
+    re.search(r'(\d\.){3,}', tag).group(0)
+  except:
+    result += 2
+    version = 'UNKNOWN_VERSION'
 
-setup(name='frenetiq_crawler',
-      version=version,
-      author='Daniel Kiss',
-      author_email='littlesnorrboy@gmail.com',
-      url='https://github.com/snorrwe/frenetiq-crawler',
-      description="A simple web crawling module",
-      long_description=home_page,
-      license="MIT",
-      package_dir={'':'src'},
-      packages=find_packages('src'),
-      install_requires=['requests>=2.2.1'],
-      entry_points={
-          'console_scripts': [
-              'sample=sample:main',
-          ],
-      },
-      python_requires='>=2.7, >=3.4'
-     )
+  setup(name='frenetiq_crawler',
+        version=version,
+        author='Daniel Kiss',
+        author_email='littlesnorrboy@gmail.com',
+        url='https://github.com/snorrwe/frenetiq-crawler',
+        description="A simple web crawling module",
+        long_description=home_page,
+        license="MIT",
+        package_dir={'':'src'},
+        packages=find_packages('src'),
+        install_requires=['requests>=2.2.1'],
+        entry_points={
+            'console_scripts': [
+                'sample=sample:main',
+            ],
+        },
+        python_requires='>=2.7, >=3.4'
+       )
+
+  return result
+
+if __name__ == '__main__':
+  result = main()
+  sys.exit(result)
