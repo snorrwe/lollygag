5d628a7f9505c4f8f81e4ae722005d32ce3cf80c
Daniel Kiss <littlesnorrboy@gmail.com>
2017-08-30 19:24:42 +0200

    Move services logic to services.py

--- src/lollygag/services/__init__.py
+++ src/lollygag/services/__init__.py
@@ -1,35 +1,4 @@
-import sys
-import argparse
-import threading
-import requests
-from lollygag.crawler.link_crawler import LinkCrawler
-from lollygag.services.config_service import ConfigService
-from lollygag.services.print_service import PrintService
-from lollygag.services.work.work_service import WorkService
-from lollygag.dependency_injection.inject import Inject
-try:
-    import Queue
-except ImportError:
-    import queue as Queue
+from lollygag.services.services import Services as S, register_services as rs
 
-__Services = {}
-
-class Services(object):
-    requests = requests
-    crawler_factory = LinkCrawler
-    argparse = argparse.ArgumentParser
-    config_service = ConfigService
-    log_service = PrintService
-    work_service =  WorkService
-    threading =  threading
-    queue = Queue.Queue
-    logging_output = sys.stdout
-
-    def __init__(self):
-        self.__dict__ = __Services
-
-def register_services(services=None):
-    assert services is dict or services is None
-    if not services:
-        services = Services.__dict__
-    Inject.register_features(**services)
+Services = S
+register_services = rs
