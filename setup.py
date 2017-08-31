190ffc2ac34fda078d92123306f75a4249a99d38
Daniel Kiss <littlesnorrboy@gmail.com>
2017-09-01 00:51:34 +0200

    Create a .version file in the package, that holds metadata about the current version

--- setup.py
+++ setup.py
@@ -1,42 +1,44 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-home_page = """
-Lollygag
-========
+import os
+import sys
+import json
 
-About
------
+this = os.path.dirname(os.path.realpath(__file__))
 
-See: https://github.com/snorrwe/lollygag
-
-Supported Python versions:
-   -  Python 2.7
-   -  Python 3.6
-   -  Pypy
-   -  Pypy 3
-
-Installation
-------------
-
-``pip install lollygag``
-"""
+version_info_path = os.path.join(this, "lollygag", ".version")
+try:
+    with open(version_info_path, 'r') as f:
+        version_info = json.loads(f.read())
+except IOError:
+    print("\n.version file was not found!\n")
+    raise
+except:
+    print("Unexpected error occured while getting .version information!")
+    a,b,c = sys.exc_info()
+    print(a)
+    print(b)
+    print(c)
+    sys.exit(1)
 
 def main():
-    global home_page
+  global home_page
 
-    setup(name='lollygag',
+  setup(name='lollygag',
         author='Daniel Kiss',
+        version=version_info["version"],
         author_email='littlesnorrboy@gmail.com',
         url='https://github.com/snorrwe/lollygag',
         description="A simple web crawling module",
-        long_description=home_page,
+        long_description=version_info["home_page"],
         license="MIT",
         package_dir={'lollygag':'lollygag'},
         packages=find_packages('.'),
+        package_data={'lollygag': ['.version']},
         install_requires=['requests>=2.2.1'],
         tests_require=['tl.testing==0.5', 'pylint==1.7.2']
-       )
+  )
 
 if __name__ == '__main__':
   main()
