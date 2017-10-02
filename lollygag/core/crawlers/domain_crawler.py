01878018b7a58d6338bee972da18d2a65bed9d04
Daniel Kiss <littlesnorrboy@gmail.com>
2017-10-02 08:05:20 +0200

    Add docstrings to DomainCrawler methods

--- lollygag/core/crawlers/domain_crawler.py
+++ lollygag/core/crawlers/domain_crawler.py
@@ -12,16 +12,25 @@ class DomainCrawler(Crawler):
         super(DomainCrawler, self).__init__(url)
 
     def initialize_status(self, urls):
+        """
+        Extend the base initialize_status to store the domain being crawled.
+        """
         self.domain = get_domain(urls[0])
         super(DomainCrawler, self).initialize_status(urls)
 
     def process_link(self, origin, link):
+        """
+        Extend the base process_link method to only return links that are on the same domain.
+        """
         result = super(DomainCrawler, self).process_link(origin, link)
         if result is None or get_domain(result) != self.domain:
             return None
         return result
 
     def crawl(self, url=None):
+        """
+        Extend the base crawl method to assert if self.domain exists
+        """
         if url:
             self.reset(url)
         assert self.domain, "Cannot start crawling without a domain!"
