d34803b66a7eeaca2193baf2d99317723a60dd38
Daniel Kiss <littlesnorrboy@gmail.com>
2017-10-02 08:15:37 +0200

    Add the most commonly used classes to the main __init__.py. So clients need less import lines to use the library.

--- lollygag/__init__.py
+++ lollygag/__init__.py
@@ -1,4 +1,8 @@
 from lollygag.main import run as run
-
-#def run(**kwargs):
-#    r(**kwargs)
+from lollygag.core.crawlers.domain_crawler import DomainCrawler as DomainCrawler
+from lollygag.core.crawlers.crawler import Crawler as Crawler
+from lollygag.core.crawlers.mapper_crawler import MapperCrawler as MapperCrawler
+from lollygag.services.services import Services as Services
+from lollygag.dependency_injection.inject import Inject as Inject
+from lollygag.core.parsers.link_parser import ParseResult as ParseResult
+from lollygag.core.parsers.link_parser import LinkParser as LinkParser
