882213f9728319709f80c3d2bc3f9d9bd47f7bf2
Daniel Kiss <littlesnorrboy@gmail.com>
2018-06-24 10:54:20 +0200

    Add query tests

--- test/test_html_queries.py
+++ test/test_html_queries.py
@@ -1,15 +1,99 @@
 from lollygag.query import compile_query, query_html
 
 some_html = """
-<asd-node style='boi' foo='bar'>
-    <some-node foo='bar'>
+<asd-node style="boi" foo="bar">   <some-node foo="baz">
+                          asdkjaksdjaskjdjs
+</asd-node>
+<asd-node>
+                            </asd-node>
+<li>
+    <i>
+        some asd data   </i>
+</li>
+"""
+
+
+def test_data_query_regex():
+    html = """
+<ul>
+    <li>
+        something interesting
+    </li>
+    <li>
+        some item
+    </li>
+    <li>
+        something interesting
+    </li>
+    <li>
+        some item
+    </li>
+    <li>
+        something interesting
+    </li>
+</ul>
+    """
+    query = compile_query({"data": "inter.*"})
+    result = query_html(html, query)
+    assert result
+    assert len(result) == 3
+    for r in result:
+        assert str(r) == """<li>
+    something interesting
+</li>
+"""
+
+
+def test_name_query_regex():
+    query = compile_query({"name": "some.*"})
+    result = query_html(some_html, query)
+    assert result
+    assert len(result) == 1
+
+    res = str(result[0])
+    print(res)
+    assert res == """<some-node foo="baz">
+    asdkjaksdjaskjdjs
+</some-node>
+"""
+
+
+def test_attribute_query_regex():
+    query = compile_query({"attribute": {"name": "foo", "value": "ba."}})
+    result = query_html(some_html, query)
+    assert result
+    assert len(result) == 2
+
+    res = str(result[0])
+    print(res)
+    assert res == """<asd-node style="boi" foo="bar">
+    <some-node foo="baz">
         asdkjaksdjaskjdjs
     </some-node>
 </asd-node>
-<asd-node>
+"""
+
+    res = str(result[1])
+    print(res)
+    assert res == """<some-node foo="baz">
+    asdkjaksdjaskjdjs
+</some-node>
+"""
+
+
+def test_attribute_query():
+    query = compile_query({"attribute": {"name": "foo", "value": "bar"}})
+    result = query_html(some_html, query)
+    assert result
+    assert len(result) == 1
+
+    res = str(result[0])
+    print(res)
+    assert res == """<asd-node style="boi" foo="bar">
+    <some-node foo="baz">
+        asdkjaksdjaskjdjs
+    </some-node>
 </asd-node>
-<li>
-</li>
 """
 
 
@@ -18,7 +102,7 @@ def test_or_query():
         "or": ({
             "attribute": {
                 "name": "foo",
-                "value": ".*"
+                "value": "bar"
             },
             "name": "asd-node",
         }, {
@@ -27,10 +111,25 @@ def test_or_query():
     })
     result = query_html(some_html, query)
     assert result
-    assert len(result) == 2
-    def a(r):
-        print(str(r))
-        for c in r.get_children():
-            a(c)
-    list(a(i) for i in result)
-    assert 0, result
+    assert len(result) == 3
+
+    res = str(result[0])
+    print(res)
+    assert res == """<asd-node style="boi" foo="bar">
+    <some-node foo="baz">
+        asdkjaksdjaskjdjs
+    </some-node>
+</asd-node>
+"""
+    res = str(result[1])
+    print(res)
+    assert res == """<some-node foo="baz">
+    asdkjaksdjaskjdjs
+</some-node>
+"""
+    res = str(result[2])
+    print(res)
+    assert res == """<i>
+    some asd data
+</i>
+"""
