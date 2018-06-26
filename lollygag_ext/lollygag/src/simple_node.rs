5d23a05f5158c00dc5597d996398b04332790037
Daniel Kiss <littlesnorrboy@gmail.com>
2018-06-26 13:03:40 +0200

    Add html5ever::node mapping

--- lollygag_ext/lollygag/src/simple_node.rs
+++ lollygag_ext/lollygag/src/simple_node.rs
@@ -1,16 +1,17 @@
-// use std::mark::{Send, Sync};
+use html5ever::rcdom::{Handle, NodeData as Html5NodeData};
 
 pub struct SimpleHtmlNode {
-    data: NodeData,
-    children: Vec<SimpleHtmlNode>,
+    pub data: NodeData,
+    pub children: Vec<SimpleHtmlNode>,
 }
 
 pub enum NodeData {
     Element {
         name: String,
-        attributes: (String, String),
+        attributes: Vec<(String, String)>,
     },
     Text(String),
+    Unknown,
 }
 
 unsafe impl Send for SimpleHtmlNode {}
@@ -21,6 +22,41 @@ impl SimpleHtmlNode {
         SimpleHtmlNode {
             data: data,
             children: children,
+        }
+    }
+
+    pub fn from_html5ever_node(node: &Handle) -> SimpleHtmlNode {
+        match node.data {
+            Html5NodeData::Element {
+                ref name,
+                ref attrs,
+                ..
+            } => {
+                let mut children = vec![];
+                for child in node.children.borrow().iter() {
+                    children.push(SimpleHtmlNode::from_html5ever_node(child));
+                }
+                let mut attributes = vec![];
+                for attr in attrs.borrow().iter() {
+                    let attribute = (attr.name.local.to_string(), attr.value.to_string());
+                    attributes.push(attribute);
+                }
+                SimpleHtmlNode {
+                    data: NodeData::Element {
+                        name: name.local.to_string(),
+                        attributes: attributes,
+                    },
+                    children: children,
+                }
+            }
+            Html5NodeData::Text { ref contents, .. } => SimpleHtmlNode {
+                data: NodeData::Text(contents.borrow().to_string()),
+                children: vec![],
+            },
+            _ => SimpleHtmlNode {
+                data: NodeData::Unknown,
+                children: vec![],
+            },
         }
     }
 }
