21e9379d99b37aee0345df485a89f606cddd0d6c
Daniel Kiss <littlesnorrboy@gmail.com>
2018-06-29 18:50:20 +0200

    Add final deployment on tags

--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+from setuptools.command.install import install
 from setuptools import setup, find_packages
+
 try:
     from setuptools_rust import RustExtension, Binding
 except ImportError:
-    import sys
     import subprocess
     subprocess.check_call(
         [sys.executable, '-m', 'pip', 'install', 'setuptools_rust'])
     from setuptools_rust import RustExtension, Binding
 
+VERSION = '2.0.0-dev'
 DEPENDENCIES = ['requests>=2.2.1']
 TEST_DEPENDENCIES = ['pytest']
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = f"Git tag: {tag} does not match the version of this app: {VERSION}"
+            sys.exit(info)
+
 
 if __name__ == '__main__':
     setup(
         name='lollygag',
         author='Daniel Kiss',
-        version="2.0.0",
+        version=VERSION,
         author_email='littlesnorrboy@gmail.com',
         url='https://github.com/snorrwe/lollygag',
         description="A simple web crawling module",
@@ -33,4 +50,7 @@ if __name__ == '__main__':
         ],
         tests_require=TEST_DEPENDENCIES,
         extras_require={'ci': TEST_DEPENDENCIES},
-    )
+        python_requires='>=3',
+        cmdclass={
+            'verify': VerifyVersionCommand,
+        })
